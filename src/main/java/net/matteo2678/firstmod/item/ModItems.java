63fc7b03a3d7f73946f8489752cf1c06ddc2489a
Matteo <matteo.feyaerts@gmail.com>
2022-06-05 23:02:25 +0200

    in progress hihihihaw item

---
+++ src/main/java/net/matteo2678/firstmod/item/ModItems.java
@@ -0,0 +1,23 @@
+package net.matteo2678.firstmod.item;
+
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.matteo2678.firstmod.FirstMod;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
+
+public class ModItems {
+
+    public static final Item HIHIHIHAW = registerItem("hihihihaw",
+        new Item(new FabricItemSettings().group(ItemGroup.MISC)));
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registry.ITEM, new Identifier(FirstMod.MOD_ID, name), item);
+    }
+
+    public static void registerModItems() {
+        FirstMod.LOGGER.info("Registering Mod Items for " + FirstMod.MOD_ID);
+    }
+}

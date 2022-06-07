83b66bdc8aad46c97c5f427683b6a08cf6d29701
Matteo <matteo.feyaerts@gmail.com>
2022-06-07 20:49:20 +0200

    added hihihihaw item

--- src/main/java/net/matteo2678/firstmod/FirstMod.java
+++ src/main/java/net/matteo2678/firstmod/FirstMod.java
@@ -6,7 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class FirstMod implements ModInitializer {
-	public  static final String MOD_ID = "tutorialmod";
+	public  static final String MOD_ID = "firstmod";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override

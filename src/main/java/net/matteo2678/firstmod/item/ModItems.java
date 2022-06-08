f744a17f5f9a2885c6e80ea2dca1190080169241
Matteo <matteo.feyaerts@gmail.com>
2022-06-08 21:51:17 +0200

    added dowsing rod

--- src/main/java/net/matteo2678/firstmod/item/ModItems.java
+++ src/main/java/net/matteo2678/firstmod/item/ModItems.java
@@ -2,6 +2,7 @@ package net.matteo2678.firstmod.item;
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.matteo2678.firstmod.FirstMod;
+import net.matteo2678.firstmod.item.custom.DowsingRodItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
@@ -11,7 +12,11 @@ import net.minecraft.util.registry.Registry;
 public class ModItems {
 
     public static final Item HIHIHIHAW = registerItem("hihihihaw",
-        new Item(new FabricItemSettings().group(ModItemGroup.FIRSTMOD)));
+            new Item(new FabricItemSettings().group(ModItemGroup.FIRSTMOD)));
+
+    public static final Item DOWSING_ROD = registerItem("dowsing_rod",
+            new DowsingRodItem(new FabricItemSettings().group(ModItemGroup.FIRSTMOD)));
+
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registry.ITEM, new Identifier(FirstMod.MOD_ID, name), item);

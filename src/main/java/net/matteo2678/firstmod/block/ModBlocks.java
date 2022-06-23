9f8d21b00be0b84e68591972efd5477cc4594e8e
Matteo <matteo.feyaerts@gmail.com>
2022-06-23 15:47:40 +0200

    added speedyblock

--- src/main/java/net/matteo2678/firstmod/block/ModBlocks.java
+++ src/main/java/net/matteo2678/firstmod/block/ModBlocks.java
@@ -3,6 +3,7 @@ package net.matteo2678.firstmod.block;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.matteo2678.firstmod.FirstMod;
+import net.matteo2678.firstmod.block.custom.SpeedyBlock;
 import net.matteo2678.firstmod.item.ModItemGroup;
 import net.minecraft.block.Block;
 import net.minecraft.block.Material;
@@ -15,7 +16,11 @@ import net.minecraft.util.registry.Registry;
 public class ModBlocks {
 
     public static final Block HIHIHIHAW_BLOCK = registerBlock("hihihihaw_block",
-            new Block(FabricBlockSettings.of(Material.CACTUS).strength(1).requiresTool()), ModItemGroup.FIRSTMOD);
+            new Block(FabricBlockSettings.of(Material.STONE).strength(1).requiresTool()), ModItemGroup.FIRSTMOD);
+
+    public static final Block SPEEDY_BLOCK = registerBlock("speedy_block",
+            new SpeedyBlock(FabricBlockSettings.of(Material.METAL).strength(1).requiresTool()), ModItemGroup.FIRSTMOD);
+
 
     private static Block registerBlock(String name, Block block, ItemGroup group) {
         registerBlockItem(name, block, group);
